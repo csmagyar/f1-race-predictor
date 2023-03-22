0683b16211e24b151eb6314cf4eaee949703abab
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-22 21:35:39 +0100

    add routing for calendar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
 
-// const appRoutes: Routes = [
-//   {
-//     path: '', redirectTo: '/', pathMatch: 'full'
-//   }
-// ]
+const appRoutes: Routes = [
+  {
+    path: '', redirectTo: '/', pathMatch: 'full'
+  },
+  {
+    path: 'calendar', component: CalendarComponent
+  }
+]
 
 @NgModule({
   imports: [
