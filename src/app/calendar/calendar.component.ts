39f98e6b7a30838c22d70b7367cec31274fc7fa5
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-22 21:50:46 +0100

    fetch calendar from API

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DataService } from '../shared/data.service';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.css']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit, OnDestroy {
+
+  raceCalendar: any = {};
+  raceCalendarLoading: boolean = true;
+  raceCalendarSubscription: Subscription = new Subscription();
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.raceCalendarSubscription = this.dataService.getCalendar().subscribe(results => {
+      this.raceCalendar = results.MRData.RaceTable;
+      console.log(this.raceCalendar);
+      this.raceCalendarLoading = false;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.raceCalendarSubscription.unsubscribe();
+  }
 
 }
