4452c24ec90ba5dde8bc9c46520b19e66b0a1d91
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-25 00:36:49 +0100

    change the appeareance of calendar items

--- src/app/calendar/calendar-item/calendar-item.component.ts
+++ src/app/calendar/calendar-item/calendar-item.component.ts
@@ -1,10 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-calendar-item',
   templateUrl: './calendar-item.component.html',
   styleUrls: ['./calendar-item.component.css']
 })
-export class CalendarItemComponent {
+export class CalendarItemComponent implements OnInit {
+  
   @Input() raceDetails: any;
+  hovering = false;
+  isPastDate: boolean = false;
+
+  ngOnInit(): void {
+    // if current date is greater than race date, then return false
+    this.isPastDate = new Date().getTime() > new Date(`${this.raceDetails.date} ${this.raceDetails.time}`).getTime();
+  }
+  
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.hovering = true;
+  }
+
+  @HostListener('mouseleave')
+  onMouseLeave() {
+    this.hovering = false;
+  }
+
 }
