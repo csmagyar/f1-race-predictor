22dd99d855b55dc5688edbf62bbb7d76cb6de32c
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-28 22:46:39 +0200

    make authentication work

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { AuthResponseData } from './responseDate.model';
 
 @Component({
   selector: 'app-auth',
@@ -7,13 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
   isLogin = true;
+  isLoading = false;
+  error: string | null = null;
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onToggleForm() {
     this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) return;
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    let authObs: Observable<any>;
+
+    this.isLoading = true;
+
+    if (this.isLogin) {
+      authObs = this.authService.login(email,password);
+    } else {
+      authObs = this.authService.signup(email,password);
+    }
+
+    authObs.subscribe(resData => {
+      console.log(resData);
+      this.isLoading = false;
+      this.router.navigate(['/calendar']);
+    }, errorMessage => {
+      this.error = errorMessage;
+      console.log(errorMessage);
+      this.isLoading = false;
+    });
+
+    form.reset();
   }
 }
