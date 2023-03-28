437118f61dd264cb4c5bc6720dd5968ac57d0060
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-28 22:49:23 +0200

    secure specific routes with a guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 import { CalendarComponent } from './calendar/calendar.component';
 import { ResultComponent } from './result/result.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/auth', pathMatch: 'full' },
-  { path: 'calendar', component: CalendarComponent },
-  { path: 'results', children: [
+  { path: '', redirectTo: '/auth', pathMatch: 'full'},
+  { path: 'calendar', component: CalendarComponent, canActivate: [AuthGuard] },
+  { path: 'results', canActivate: [AuthGuard], children: [
       {path: ':year/:round', component: ResultComponent,},
     ]
   },
