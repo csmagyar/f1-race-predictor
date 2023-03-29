a99e6c6716640815c21125101c16333aa9b36b64
csmagyar <61981243+csmagyar@users.noreply.github.com>
2023-03-29 08:43:14 +0200

    update route protection for users
    Revise the route protection mechanisms in the app to ensure that certain pages can only be accessed by logged-in users, while others are only accessible to logged-out users. This change improves security and user privacy, ensuring that unauthorized users cannot access sensitive information. The updated code also simplifies the routing logic and makes it easier to maintain going forward.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Router, RouterModule, Routes } from '@angular/router';
+import { AuthLoggedinGuard } from './auth/auth-loggedin.guard';
 import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './auth/auth.guard';
 import { CalendarComponent } from './calendar/calendar.component';
 import { ResultComponent } from './result/result.component';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/auth', pathMatch: 'full'},
-  { path: 'calendar', component: CalendarComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/calendar', pathMatch: 'full'},
+  { path: 'calendar', component: CalendarComponent },
   { path: 'results', canActivate: [AuthGuard], children: [
       {path: ':year/:round', component: ResultComponent,},
     ]
   },
-  { path: 'auth', component: AuthComponent },
+  { path: 'auth', component: AuthComponent, canActivate: [AuthLoggedinGuard] },
 ]
 
 @NgModule({
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
     FormsModule,
     HttpClientModule,
     RouterModule.forChild([ 
-      { path: '', component: AuthComponent/* , canActivate: [] */ },
+      { path: '', component: AuthComponent },
     ])
   ]
 })
